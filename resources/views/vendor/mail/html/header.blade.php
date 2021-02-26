9b007a4d49631d2a12e41ff03b8559d7f004b9f9
Vitor Medeiros <vitordaniel602@gmail.com>
2021-02-26 14:05:51 -0300

    Atualização no envio de email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{route('home')}}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{asset('DigitalFuelStation/logo.png')}}" class="logo" alt="Digital Fuel Station Logo">
+Digital Fuel Station
 @else
 {{ $slot }}
 @endif
